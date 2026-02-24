8f40fd3eed47a54938164b926b29c27d7c7a7f5c
andrei <andreimoreiraneves@gmail.com>
2026-02-23 21:56:13 -0300

    Organização: movidos arquivos de teste e removido backup

--- backend/backup/loja.js
+++
@@ -1,8 +0,0 @@
-// backend/src/routes/loja.js
-const express = require('express');
-const lojaController = require('../controllers/lojaController');
-const router = express.Router();
-
-router.get('/loja/status/:subdominio', lojaController.verificarStatusLoja);
-
-module.exports = router;
